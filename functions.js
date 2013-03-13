a9e47ac0403410fdbe324c4d924f8baffb8a06d1
mus cem <muscem1@hotmail.com>
2013-03-14 01:02:03 +0200

    new

--- functions.js
+++ functions.js
@@ -261,12 +261,12 @@ function chooseLang(lang){
 //lang="en";	//Deneme amaçli, silinecek
 gameLang=lang;
 
-//gameLangChange();	//tarayicida çalismayi önler
+gameLangChange();	//tarayicida çalismayi önler
 alert("sayfa degis"+gameLang);
 $("#langSelect").val(gameLang);
 $("#langSelect").trigger("change");
 
-$.mobile.changePage( $("#pagePlayers") , { transition: "slide"} );
+$.mobile.changePage( $("#pagePlayers") );
 }
 /* pageLang kodu sonu */
 
